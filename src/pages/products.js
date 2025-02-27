43decdfee7dfcc54b67b8063619d670ca886d3ef
seojiho <happyluck0426@gmail.com>
2025-02-27 13:54:37 +0900

    feat: 제품 검색 기능 구현

--- src/pages/products.js
+++ src/pages/products.js
@@ -6,6 +6,7 @@ import display from "../displayProducts.js";
 import fetchProducts from "../fetchProducts.js";
 import { setupStore, store } from "../store.js";
 import { getElement } from "../utils.js";
+import setupSearch from "../filters/search.js";
 
 const init = async () => {
     const loadingEl = getElement('.page-loading');
@@ -16,6 +17,9 @@ const init = async () => {
 
     // 상품들을 보여주기
     display(store, getElement('.products-container'));
+
+    
+    setupSearch(store);
 
     loadingEl.style.display = 'none';
 }
