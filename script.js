0c51807de32dbf4ae8ffca9fb71d9c1b92670387
seojiho <happyluck0426@gmail.com>
2025-02-27 12:40:27 +0900

    feat: 메뉴 사이드바 기능 구현

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ import { setupStore } from "./src/store.js";
 import display from "./src/displayProducts.js";
 import { getElement } from "./src/utils.js";
 
+import './src/toggleSidebar.js';
+
 const init = async () => {
     const products = await fetchProducts();
 
---
+++ src/toggleSidebar.js
@@ -0,0 +1,14 @@
+//  메뉴 클릭 시 사이드 바
+import { getElement } from "./utils.js";
+
+const toggleNav = getElement('.toggle-nav');
+const sidebarOverlay = getElement('.sidebar-overlay');
+const closeBtn = getElement('.sidebar-close');
+
+toggleNav.addEventListener('click', () => {
+    sidebarOverlay.classList.add('show');
+})
+
+closeBtn.addEventListener('click', () => {
+    sidebarOverlay.classList.remove('show');
+})
