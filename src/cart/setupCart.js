db5f9cf691725e2283aa480bc9b5aff592de2ab2
seojiho <happyluck0426@gmail.com>
2025-02-27 13:19:48 +0900

    feat: 장바구니 상세기능 구현

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -3,7 +3,9 @@ import { formatPrice, getElement, getStorageItem, setStorageItem } from "../util
 import addToCartDOM from "./addToCartDom.js";
 import { openCart } from "./toggleCart.js";
 
+const cartItemCountEl = getElement('.cart-item-count');
 const cartItemsEl = getElement('.cart-items');
+const cartTotalEl = getElement('.cart-total');
 
 let cart = getStorageItem('cart');
 
@@ -28,3 +30,103 @@ export const addToCart = (id) => {
         itemEl.textContent = amount;
     }
 }
+
+function displayCartItemsDOM() {
+    cart.forEach((cartItem) => {
+        addToCartDOM(cartItem);
+    })
+}
+
+function displayCartItemCount() {
+    const amount = cart.reduce((acc, curr) => {
+        return (acc += curr.amount)
+    }, 0);
+    cartItemCountEl.textContent = amount;
+}
+
+function displayCartTotal() {
+    let total = cart.reduce((acc, curr) => {
+        return (acc += curr.price * curr.amount)
+    }, 0);
+
+    cartTotalEl.textContent = `Total: ${formatPrice(total)}`;
+}
+
+function removeItem(id) {
+    cart = cart.filter((cartItem) => cartItem.id !== id)
+}
+
+
+function increaseAmount(id) {
+    let newAmount;
+
+    cart = cart.map((cartItem) => {
+        if (cartItem.id === id) {
+            newAmount = cartItem.amount + 1
+            cartItem = { ...cartItem, amount: newAmount }
+        }
+        return cartItem
+    })
+    return newAmount;
+}
+
+function decreaseAmount(id) {
+    let newAmount;
+
+    cart = cart.map((cartItem) => {
+        if (cartItem.id === id) {
+            newAmount = cartItem.amount - 1;
+            cartItem = { ...cartItem, amount: newAmount }
+        }
+        return cartItem;
+    })
+    return newAmount;
+}
+
+function setupCartFunctionality() {
+    cartItemsEl.addEventListener('click', (e) => {
+        const element = e.target;
+        const parent = e.target.parentElement;
+        const id = e.target.dataset.id;
+        const parentID = e.target.parentElement.dataset.id;
+
+
+        // remove
+        if (element.classList.contains('cart-item-remove-btn')) {
+            removeItem(id);
+
+            element.parentElement.parentElement.remove();
+        }
+        // increase
+        if (parent.classList.contains('cart-item-increase-btn')) {
+            const newAmount = increaseAmount(parentID);
+            parent.nextElementSibling.textContent = newAmount
+        }
+        // decrease
+        if (parent.classList.contains('cart-item-decrease-btn')) {
+            const newAmount = decreaseAmount(parentID);
+            if (newAmount === 0) {
+                removeItem(parentID);
+                parent.parentElement.parentElement.remove();
+            } else {
+                parent.previousElementSibling.textContent = newAmount;
+            }
+
+        }
+        displayCartItemCount();
+        displayCartTotal();
+        setStorageItem('cart', cart);
+    })
+}
+
+const init = () => {
+    displayCartItemCount();
+
+    displayCartTotal();
+
+    displayCartItemsDOM();
+
+    setupCartFunctionality();
+}
+
+init();
