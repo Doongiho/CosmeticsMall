e04e8b692ee71cee3fa6055b93c33ed846e0a12e
seojiho <happyluck0426@gmail.com>
2025-02-27 13:46:59 +0900

    feat: 제품 목록  데이터 로드 및 표시 기능 구현

---
+++ src/pages/products.js
@@ -0,0 +1,23 @@
+import '../cart/toggleCart.js';
+import '../toggleSidebar.js';
+import '../cart/setupCart.js';
+
+import display from "../displayProducts.js";
+import fetchProducts from "../fetchProducts.js";
+import { setupStore, store } from "../store.js";
+import { getElement } from "../utils.js";
+
+const init = async () => {
+    const loadingEl = getElement('.page-loading');
+
+    // 상품들 데이터 가져오기
+    const products = await fetchProducts();
+    setupStore(products);
+
+    // 상품들을 보여주기
+    display(store, getElement('.products-container'));
+
+    loadingEl.style.display = 'none';
+}
+
+init();
